18c92086070bb84d62c05ef5c7818581d55f611a
Ronashovkova <rona_t@icloud.com>
2020-03-07 00:48:58 +0200

    added code into PositionController

--- src/main/java/com/project/shop/controller/PositionController.java
+++
@@ -1,9 +0,0 @@
-package com.project.shop.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/position")
-public class PositionController {
-}
