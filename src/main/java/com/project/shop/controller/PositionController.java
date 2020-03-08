f2ea7d23b9a2a1ecb19fa24701bb2f2c1a69f87d
Ronashovkova <rona_t@icloud.com>
2020-03-08 06:24:39 +0200

    added update into PositionController

--- src/main/java/com/project/shop/controller/PositionController.java
+++ src/main/java/com/project/shop/controller/PositionController.java
@@ -1,22 +1,26 @@
 package com.project.shop.controller;
 
+import com.project.shop.dto.CategoryDto;
 import com.project.shop.dto.PositionDto;
-import com.project.shop.model.Category;
 import com.project.shop.model.Position;
+import com.project.shop.service.impl.CategoryServiceImpl;
 import com.project.shop.service.impl.PositionServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+
 @RestController
 @RequestMapping("/position")
 public class PositionController {
     private PositionServiceImpl positionService;
+    private CategoryServiceImpl categoryService;
 
     @Autowired
-    public PositionController(PositionServiceImpl positionService) {
+    public PositionController(PositionServiceImpl positionService, CategoryServiceImpl categoryService) {
         this.positionService = positionService;
+        this.categoryService = categoryService;
     }
 
     @GetMapping("/")
@@ -24,9 +28,22 @@ public class PositionController {
         return ResponseEntity.status(HttpStatus.OK).body(positionService.getAll());
     }
 
-    @GetMapping("/{id}") //не знаю як тут категорію задіяти згідно таски
-    public ResponseEntity<PositionDto> getPositionById(@PathVariable Long id, @RequestParam Category category) {
-        return ResponseEntity.status(HttpStatus.OK).body(positionService.getById(id));
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoryDto> getAllPositionsByCategoryId(@PathVariable Long id) {
+
+
+        return ResponseEntity.status(HttpStatus.OK).body(categoryService.getById(id));
+        //дістала по id категорію, а як з неї всі позиції вибрати - не знаю.
+        //List<Position> positionsList = positionService.getAll();
+
+     /*   @OneToMany
+        @JoinTable(
+                name = "position_category",
+                joinColumns = @JoinColumn(name = "position_id"),
+                inverseJoinColumns = @JoinColumn(name = "category_Id")
+        )
+        private List<Category> listCategoryId;
+        */
     }
 
     @PostMapping("/")
@@ -35,12 +52,12 @@ public class PositionController {
         return ResponseEntity.status(HttpStatus.CREATED).body(newPosition);
     }
 
-    @PutMapping("/{id}") // створити в сервісі окремий метод для апдейту по id?
-    public ResponseEntity<String> updatePosition(@PathVariable Long id, @RequestBody Position position) {
-        positionService.saveOrUpdate(position);
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body("Position " + position.toString() + " was updated successfully");
+    @PutMapping("/{id}")
+    public ResponseEntity updatePosition(@PathVariable Long id, @RequestBody Position position) {
+        PositionDto newPosition = positionService.getById(id);
+        return newPosition != null
+                ? new ResponseEntity<>(position, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @DeleteMapping("/{id}")
--- src/main/java/com/project/shop/controller/StatisticsController.java
+++ src/main/java/com/project/shop/controller/StatisticsController.java
@@ -1,9 +1,19 @@
 package com.project.shop.controller;
 
+import com.project.shop.service.impl.AccountServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/statistics")
 public class StatisticsController {
+        private AccountServiceImpl accountService;
+
+    @Autowired
+    public StatisticsController(AccountServiceImpl accountService) {
+        this.accountService = accountService;
+    }
+
+
 }
